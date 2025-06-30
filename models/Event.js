6fde9b1f8a44582e7f6458dc35b6aef34cb4b9f4
Raja khan <129713278+rajak82001@users.noreply.github.com>
2025-06-30 23:25:49 +0530

    Delete models directory

--- models/Event.js
+++
@@ -1,9 +0,0 @@
-// models/Event.js
-const mongoose = require('mongoose');
-const eventSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  date: Date,
-  location: String
-});
-module.exports = mongoose.model('Event', eventSchema);
--- models/Registration.js
+++
@@ -1,9 +0,0 @@
-// models/Registration.js
-const mongoose = require('mongoose');
-const registrationSchema = new mongoose.Schema({
-  eventId: String,
-  name: String,
-  email: String,
-  phone: String
-});
-module.exports = mongoose.model('Registration', registrationSchema);
